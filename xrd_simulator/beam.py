8b6c32c573c16e0501fb04563307485a9a6a97d6
Axel Henningsson <nilsaxelhenningssongmail.com>
2021-07-27 20:48:20 +0200

    shape out the structure of the objects

--- xrd_simulator/beam.py
+++ xrd_simulator/beam.py
@@ -1,3 +1,13 @@
 """The beam class defines an X-ray as a 3d spatial (convex) object by 
 taking a set of points and intepreting them as the convex hull of the beam
 cross section. The beam is then extruded in a given direction towards infinity.""" 
+
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
+
+class Beam(object):
+
+    def __init__(self, vertices, wavelength ):
+        self.convex_beam_hull = ConvexHull(vertices)
+        self.wavelength = wavelength
--- xrd_simulator/polycrystal.py
+++ xrd_simulator/polycrystal.py
@@ -1,2 +1,25 @@
 """A Polycrystal object links a mesh to a phase-list and hold a ubi matrix to each element
 and can produce corresponding hkl indices and intensity factors for these crystals""" 
+
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
+
+class Polycrystal(object):
+
+    def __init__(self, mesh, ephase, phases ):
+        self.mesh   = mesh
+        self.ephase = ephase
+        self.phases = phases
+
+    def _get_crystal_beam_intersections(self, beam, elements):
+        """Compute convex intersection hulls of beam and selected mesh elements."""
+        pass
+
+    def _get_candidate_elements(self, beam):
+        """Get all elements that could diffract for a given illumination setting."""
+        pass
+
+    def get_scattered_rays(self, beam):
+        """Construct ScatteredRay objects for each scattering occurence and return."""
+        pass
