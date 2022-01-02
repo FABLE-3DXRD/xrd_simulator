3483d322a7b89de9e504f63609cd4a1340f80ca0
Axel Henningsson <nilsaxelhenningssongmail.com>
2022-01-02 21:35:18 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xrd_simulator",
-    version="0.0.1",
+    version="0.0.2",
     author="Axel Henningsson",
     author_email="nilsaxelhenningsson@gmail.com",
     description="Tools for diffraction simulation of s3dxrd type experiments.",
