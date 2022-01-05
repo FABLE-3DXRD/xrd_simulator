9b113d69a4897ab095136c24a97151f3c6e3c09e
Axel Henningsson <nilsaxelhenningssongmail.com>
2022-01-05 15:10:43 +0100

    fixing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['../source/_static']
+html_logo = "logo_stripped_white.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
