06eedb0e09bc84d669c583d3ca72cb06c0a67e83
Axel Henningsson <nilsaxelhenningssongmail.com>
2022-01-05 12:24:04 +0100

    bumping version to 0.1.4 and distributing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xrd_simulator",
-    version="0.1.3",
+    version="0.1.4",
     author="Axel Henningsson",
     author_email="nilsaxelhenningsson@gmail.com",
     description="Tools for diffraction simulation of s3dxrd type experiments.",
@@ -18,17 +18,18 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.8,<3.10",
-    install_requires=["matplotlib==3.5.1",
+    python_requires="3.8",
+    install_requires=["matplotlib==3.3.1",
                       "numpy==1.21.4",
                       "meshio==5.0.2",
                       "pygalmesh==0.10.6",
-                      "scipy==1.7.3",
-                      "numba==0.54.1",
+                      "scipy==1.6.1",
+                      "numba==0.53.0",
                       "pycifrw==4.4.3"]
 )
+
+
