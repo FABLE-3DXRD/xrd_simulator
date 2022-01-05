b5d05711614b77dbc0ac509afa4d9b63ec1344b7
Axel Henningsson <nilsaxelhenningssongmail.com>
2022-01-05 13:27:37 +0100

    trying macos build

--- docs/serve_docs.py
+++ docs/serve_docs.py
@@ -5,11 +5,11 @@ import os
 import shutil
 
 out = os.system("make html")
-if out!=0:
+if out != 0:
     print("")
     print("")
     raise ValueError("Failed to build docs")
 for file in os.listdir(os.path.join("build","html")):
     if not file.startswith('_'):
-        shutil.copy2(os.path.join(os.path.join("build","html"),file), ".")
-print( "Copied all docs to path visible by github pages" )
+        shutil.copy2(os.path.join(os.path.join("build", "html"), file), ".")
+print("Copied all docs to path visible by github pages")
