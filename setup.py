237d8ec1cfced6cbcdca85991e07c86dbd47cf1c
Axel Henningsson <nilsaxelhenningssongmail.com>
2022-01-05 10:34:50 +0100

    more dependecy tweaks

--- setup.py
+++ setup.py
@@ -22,13 +22,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.8,<3.10",
     install_requires=["matplotlib==3.5.1",
-                      "numpy==1.21.5",
+                      "numpy==1.22.0",
                       "meshio==5.0.2",
                       "pygalmesh==0.10.6",
                       "scipy==1.7.3",
                       "numba==0.54.1",
                       "pycifrw==4.4.3"]
 )
-
