8892b20e0ec328399a91000d9b7a76b47538212b
Axel Henningsson <nilsaxelhenningssongmail.com>
2022-01-05 23:59:51 +0100

    bumping version to 0.2.5 and distributing

--- docs/source/examples/make_detector.py
+++ docs/source/examples/make_detector.py
@@ -1,10 +1,11 @@
 import numpy as np
 from xrd_simulator.detector import Detector
 
-pixel_size = 75.
-detector_size = pixel_size * 1024
+pixel_size_z = 75.0
+pixel_size_y = 55.0
+detector_size_z = pixel_size_z * 1024
 detector_distance = 142938.3
-d0 = np.array([detector_distance, -detector_size / 2., -detector_size / 2.])
-d1 = np.array([detector_distance,  detector_size / 2., -detector_size / 2.])
-d2 = np.array([detector_distance, -detector_size / 2.,  detector_size / 2.])
-detector = Detector(pixel_size, pixel_size, d0, d1, d2)
+d0 = np.array([detector_distance, -detector_size_z / 2., -detector_size_z / 2.])
+d1 = np.array([detector_distance, detector_size_z / 2., -detector_size_z / 2.])
+d2 = np.array([detector_distance, -detector_size_z / 2., detector_size_z / 2.])
+detector = Detector(pixel_size_z, pixel_size_y, d0, d1, d2)
