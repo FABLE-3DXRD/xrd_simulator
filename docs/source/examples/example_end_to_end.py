803190da77e8271b9a6673ee68c5ceb03eceb784
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2022-01-09 19:41:34 +0100

    README updates

--- docs/source/examples/example_end_to_end.py
+++ docs/source/examples/example_end_to_end.py
@@ -1,6 +1,5 @@
 import numpy as np
 from xrd_simulator.beam import Beam
-
 # The beam of x-rays is represented as a convex polyhedron
 # We specify the vertices in a numpy array.
 beam_vertices = np.array([
@@ -19,9 +18,7 @@ beam = Beam(
     wavelength=0.28523,
     polarization_vector=np.array([0., 1., 0.]))
 
-
 from xrd_simulator.detector import Detector
-
 # The detector plane is defined by it's corner coordinates det_corner_0,det_corner_1,det_corner_2
 detector = Detector(pixel_size_z=75.0,
                     pixel_size_y=55.0,
@@ -29,12 +26,8 @@ detector = Detector(pixel_size_z=75.0,
                     det_corner_1=np.array([142938.3, 38400., -38400.]),
                     det_corner_2=np.array([142938.3, -38400., 38400.]))
 
-
 from scipy.spatial.transform import Rotation as R
 from xrd_simulator.mesh import TetraMesh
-from xrd_simulator.phase import Phase
-from xrd_simulator.polycrystal import Polycrystal
-
 # xrd_simulator supports several ways to generate a mesh, here we
 # generate meshed solid sphere using a level set.
 mesh = TetraMesh.generate_mesh_from_levelset(
@@ -42,11 +35,13 @@ mesh = TetraMesh.generate_mesh_from_levelset(
     bounding_radius=769.0,
     max_cell_circumradius=450.)
 
+from xrd_simulator.phase import Phase
 quartz = Phase(unit_cell=[4.926, 4.926, 5.4189, 90., 90., 120.],
                sgname='P3221',  # (Quartz)
                path_to_cif_file=None  # phases can be defined from crystalographic information files
                )
 
+from xrd_simulator.polycrystal import Polycrystal
 orientation = R.random(mesh.number_of_elements).as_matrix()
 polycrystal = Polycrystal(mesh,
                           orientation,
@@ -54,9 +49,7 @@ polycrystal = Polycrystal(mesh,
                           phases=quartz,
                           element_phase_map=None)
 
-
 from xrd_simulator.motion import RigidBodyMotion
-
 motion = RigidBodyMotion(rotation_axis=np.array([0, 1/np.sqrt(2), -1/np.sqrt(2)]),
                          rotation_angle=np.radians(1.0),
                          translation=np.array([123, -153.3, 3.42]))
