fix lorentz factor normalisation

xrd_simulator/scatterer.py
@@ -70,10 +70,11 @@ class Scatterer(object):
     def lorentz_factor(self):
         """Compute the Lorentz intensity factor for a scatterer.
         """
+        #TODO: Fix this to work with the rigid body motion...
         k = self.incident_wave_vector
         kp = self.scattered_wave_vector
         theta = np.arccos(k.dot(kp) / (np.linalg.norm(k)**2)) / 2.
-        korthogonal = kp - k * kp.dot(k) / np.linalg.norm(k**2)
+        korthogonal = kp - (k * kp.dot(k) / (np.linalg.norm(k)**2))
         eta = np.arccos(
             self.rotation_axis.dot(korthogonal) /
             np.linalg.norm(korthogonal))