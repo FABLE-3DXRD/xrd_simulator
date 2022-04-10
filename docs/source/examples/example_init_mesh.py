a0ff07b8e1f2f4c6b13bdd7276383094e4afcf98
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2022-04-10 11:49:59 +0200

    updates to mesh docs

--- docs/source/examples/example_init_mesh.py
+++ docs/source/examples/example_init_mesh.py
@@ -1,10 +1,9 @@
 import numpy as np
 from xrd_simulator.mesh import TetraMesh
 
-
+# Generate mesh with 2 elements from nodal coordinates.
 nodal_coordinates = np.array([[0,0,0],[0,1,0],[1,0,0],[0,0,1],[0,0,-1]])
 element_node_map  = np.array([[0,1,2,3],[0,1,2,4]])
-# Generate meshed solid sphere using a level set.
 mesh = TetraMesh.generate_mesh_from_vertices(nodal_coordinates, element_node_map)
 
 # The mesh may be saved to disc for later usage or visualization.
