Heavy optimisation and fixed rotation axis frame splitting implemented

docs/source/examples/example_readme.py
@@ -77,7 +77,7 @@ motion = RigidBodyMotion(rotation_axis=np.array([0, 1/np.sqrt(2), -1/np.sqrt(2)]
 
 ##example: diffract
 polycrystal.diffract(beam, detector, motion)
-diffraction_pattern = detector.render(frame_number=0,
+diffraction_pattern = detector.render(frames_to_render=0,
                                         lorentz=False,
                                         polarization=False,
                                         structure_factor=False,

tests/test_beam.py
@@ -76,7 +76,7 @@ class TestBeam(unittest.TestCase):
         ch = self.beam.intersect(vertices)
         self.assertTrue(ch is None)
 
-    def test__get_proximity_intervals_exact(self):
+    def test__get_proximity_intervals(self):
 
         self.beam_vertices = np.array([
             [-500., -1., -1.],
@@ -108,26 +108,27 @@ class TestBeam(unittest.TestCase):
         sphere_radius = np.array([[2.0], [0.5]])
 
         intervals = self.beam._get_proximity_intervals(
-            sphere_centres, sphere_radius, motion, collision_detection='exact')
+            sphere_centres, sphere_radius, motion)
 
         self.assertEqual(len(intervals[0]), 2,
                          msg="Wrong number of proximity intervals")
         self.assertEqual(len(intervals[1]), 2,
                          msg="Wrong number of proximity intervals")
 
+        tol = 2. / np.degrees(motion.rotation_angle)
         for i in range(sphere_centres.shape[0]):
             # far away sphere small radii approximation:
             fraction_before_beam_leaves_sphere = np.arctan(
                 sphere_radius[i] / np.linalg.norm(sphere_centres[i])) / np.pi
             self.assertAlmostEqual(
                 intervals[i][0][0], 0, msg="Proximity interval wrong")
-            self.assertAlmostEqual(
-                intervals[i][0][1],
-                fraction_before_beam_leaves_sphere[0],
+            self.assertLessEqual(
+                np.abs(intervals[i][0][1] - 
+                fraction_before_beam_leaves_sphere[0]), tol,
                 msg="Proximity interval wrong")
-            self.assertAlmostEqual(
-                intervals[i][1][0],
-                1. - fraction_before_beam_leaves_sphere[0],
+            self.assertLessEqual(
+                np.abs(intervals[i][1][0]-(
+                1. - fraction_before_beam_leaves_sphere[0])), tol,
                 msg="Proximity interval wrong")
             self.assertAlmostEqual(
                 intervals[i][1][1],
@@ -137,8 +138,7 @@ class TestBeam(unittest.TestCase):
         # Now with rotation and translation
         motion.translation = np.array([-87.24, 34.6, 123.34])
 
-        intervals = self.beam._get_proximity_intervals(
-            sphere_centres, sphere_radius, motion)
+        intervals = self.beam._get_proximity_intervals(sphere_centres, sphere_radius, motion)
 
         self.assertEqual(len(intervals[0]), 1,
                          msg="Wrong number of proximity intervals")
@@ -158,95 +158,7 @@ class TestBeam(unittest.TestCase):
             self.assertTrue(
                 np.abs(
                     intervals[i][0][1] -
-                    fraction_before_beam_leaves_sphere) < 1. /
-                len(times),
-                msg="Proximity interval wrong")
-
-
-    def test__get_proximity_intervals_approximate(self):
-
-        self.beam_vertices = np.array([
-            [-500., -1., -1.],
-            [-500., -1., 1.],
-            [-500., 1., 1.],
-            [-500., 1., -1.],
-            [500., -1., -1.],
-            [500., -1., 1.],
-            [500., 1., 1.],
-            [500., 1., -1.]])
-        self.beam_vertices[:, 1:] = self.beam_vertices[:,
-                                                       1:] / 10000000.  # tiny beam cross section
-        self.xray_propagation_direction = np.array([1, 0, 0])
-        self.polarization_vector = np.random.rand(3,)
-        self.polarization_vector = self.polarization_vector - \
-            np.dot(self.polarization_vector, self.xray_propagation_direction) * self.xray_propagation_direction
-        self.beam = Beam(
-            self.beam_vertices,
-            self.xray_propagation_direction,
-            self.wavelength,
-            self.polarization_vector)
-
-        rotation_axis = np.array([0., 0., 1.])
-        rotation_angle = np.pi - 1e-8
-        translation = np.array([0., 0., 0.])
-        motion = RigidBodyMotion(rotation_axis, rotation_angle, translation)
-
-        sphere_centres = np.array([[400.0, 0.0, 0.0], [200.0, 0.0, 0.0]])
-        sphere_radius = np.array([[2.0], [0.5]])
-
-        intervals = self.beam._get_proximity_intervals(
-            sphere_centres, sphere_radius, motion, collision_detection='approximate')
-
-        self.assertEqual(len(intervals[0]), 2,
-                         msg="Wrong number of proximity intervals")
-        self.assertEqual(len(intervals[1]), 2,
-                         msg="Wrong number of proximity intervals")
-
-        for i in range(sphere_centres.shape[0]):
-            # far away sphere small radii approximation:
-            fraction_before_beam_leaves_sphere = np.arctan(
-                sphere_radius[i] / np.linalg.norm(sphere_centres[i])) / np.pi
-            self.assertAlmostEqual(
-                intervals[i][0][0], 0, msg="Proximity interval wrong")
-            self.assertAlmostEqual(
-                intervals[i][0][1],
-                fraction_before_beam_leaves_sphere[0],
-                msg="Proximity interval wrong")
-            self.assertAlmostEqual(
-                intervals[i][1][0],
-                1. - fraction_before_beam_leaves_sphere[0],
-                msg="Proximity interval wrong")
-            self.assertAlmostEqual(
-                intervals[i][1][1],
-                1.0,
-                msg="Proximity interval wrong")
-
-        # Now with rotation and translation
-        motion.translation = np.array([-87.24, 34.6, 123.34])
-
-        intervals = self.beam._get_proximity_intervals(
-            sphere_centres, sphere_radius, motion)
-
-        self.assertEqual(len(intervals[0]), 1,
-                         msg="Wrong number of proximity intervals")
-        self.assertEqual(len(intervals[1]), 1,
-                         msg="Wrong number of proximity intervals")
-
-        for i in range(sphere_centres.shape[0]):
-            # search numerically for the point in time when the sphere leaves
-            # the beam and compare to analytical intersection:
-            times = np.linspace(0., 1., 10000)
-            cs = np.array([motion(sphere_centres[i], t) for t in times])
-            L = np.sqrt(cs[:, 1]**2 + cs[:, 2]**2)
-            fraction_before_beam_leaves_sphere = times[np.argmin(
-                np.abs(L - sphere_radius[i]))]
-            self.assertAlmostEqual(
-                intervals[i][0][0], 0, msg="Proximity interval wrong")
-            self.assertTrue(
-                np.abs(
-                    intervals[i][0][1] -
-                    fraction_before_beam_leaves_sphere) < 1. /
-                len(times),
+                    fraction_before_beam_leaves_sphere) < tol,
                 msg="Proximity interval wrong")
 
 
@@ -268,7 +180,8 @@ class TestBeam(unittest.TestCase):
         sphere_centres = np.random.rand(10,3)
         sphere_centres[-1,:] = (100,100,100)
         sphere_radius = np.random.rand(10,)
-        mask = self.beam._get_candidate_spheres( sphere_centres, sphere_radius, motion)
+        mask, _ = self.beam._get_candidate_spheres( sphere_centres, sphere_radius, motion)
+        mask = np.sum(mask, axis=0)>0
         self.assertTrue( np.allclose(mask[0:9], True) )
         self.assertTrue( np.allclose(mask[9], False) )
 

xrd_simulator/beam.py
@@ -45,6 +45,7 @@ class Beam():
             xray_propagation_direction,
             wavelength,
             polarization_vector):
+        
         self.wave_vector = (2 * np.pi / wavelength) * xray_propagation_direction / \
             np.linalg.norm(xray_propagation_direction)
         self.wavelength = wavelength
@@ -65,7 +66,11 @@ class Beam():
         assert ch.points.shape[0] == ch.vertices.shape[0], "The provided beam vertices does not form a convex hull"
         self.vertices = beam_vertices.copy()
         self.centroid = np.mean(self.vertices, axis=0)
-        self.halfspaces = ConvexHull(self.vertices).equations
+        self.halfspaces = ConvexHull(self.vertices, qhull_options='QJ').equations
+
+        # ConvexHull triangulates, this removes hull triangles positioned the same plane
+        self.halfspaces = np.unique(self.halfspaces.round(decimals=6), axis=0) 
+
 
     def contains(self, points):
         """ Check if the beam contains a number of point(s).
@@ -103,6 +108,7 @@ class Beam():
             return ConvexHull(vertices) # Tetra completely contained by beam
 
         poly_halfspace = ConvexHull(vertices).equations
+        poly_halfspace = np.unique(poly_halfspace.round(decimals=6), axis=0) 
         combined_halfspaces = np.vstack((poly_halfspace, self.halfspaces))
 
         # Since _find_feasible_point() is expensive it is worth checking for if the polyhedra
@@ -119,7 +125,7 @@ class Beam():
                     interior_point = trial_points[i,:].flatten()
                     break
             else:
-                interior_point = self._find_feasible_point(combined_halfspaces)
+                interior_point = self._find_feasible_point(combined_halfspaces, centroid)
 
         if interior_point is not None:
             hs = HalfspaceIntersection(combined_halfspaces, interior_point)
@@ -157,7 +163,7 @@ class Beam():
         with open(path, 'rb') as f:
             return dill.load(f)
 
-    def _find_feasible_point(self, halfspaces):
+    def _find_feasible_point(self, halfspaces, element_centroid):
         """Find a point which is clearly inside a set of halfspaces (A * point + b < 0).
 
         Args:
@@ -167,29 +173,22 @@ class Beam():
             (:obj:`None`) if no point is found else (:obj:`numpy array`) point.
 
         """
-        # NOTE: from profiling: this method is slow, beware of using it
-        # unnecessarily.
-        norm_vector = np.reshape(np.linalg.norm(
-            halfspaces[:, :-1], axis=1), (halfspaces.shape[0], 1))
-        c = np.zeros((halfspaces.shape[1],))
-        c[-1] = -1
-        A = np.hstack((halfspaces[:, :-1], norm_vector))
-        b = - halfspaces[:, -1:]
         res = linprog(
-            c,
-            A_ub=A,
-            b_ub=b,
-            bounds=(
-                None,
-                None),
-            method='highs-ipm',
-            options={
-                "maxiter": 2000})
-        if res.success and res.x[-1] > 0:
-            return res.x[:-1]
+            c       =  np.zeros((3,)),
+            A_ub    =  halfspaces[:, :-1],
+            b_ub    = -halfspaces[:, -1],
+            bounds  = (None, None),
+            method  = 'highs',
+            options = {"maxiter": 5000, 'time_limit':0.1} # typical solve time is ~1-2 ms
+            )
+        if res.success: 
+            trial = res.x - (element_centroid-res.x)*0.999
+            if np.all( (halfspaces[:, :-1].dot(trial) + halfspaces[:, -1]) < -1e8 ): 
+                return trial
+            else:
+                return None
         else:
             return None
-
 
     def _get_candidate_spheres(
             self,
@@ -220,187 +219,76 @@ class Beam():
         translation = np.abs( rigid_body_motion.translation / dx )
         number_of_sampling_points = int( np.max( [np.max(translation), np.degrees(rigid_body_motion.rotation_angle), 2] ) + 1 )
         sample_times  = np.linspace(0, 1, number_of_sampling_points)
-    
-        beam_series_vertices = np.vstack([inverse_rigid_body_motion( self.vertices.T, time=time ).T for time in sample_times])
-        beam_series_halfspaces = ConvexHull( beam_series_vertices, qhull_options='QJ' ).equations
-        planes_origin_distances = beam_series_halfspaces[:, 3].reshape(beam_series_halfspaces.shape[0],1)
-        sphere_beam_distances   = beam_series_halfspaces[:,0:3].dot(sphere_centres.T) + planes_origin_distances
-        R = sphere_radius.reshape(1, sphere_radius.shape[0])
-        mask = np.all( sphere_beam_distances<R, axis=0)
-        
-        # for plane in beam:
-        # if centroid inside : add
-        # compute sphere_beam_distances
-        # if np.abs(sphere_beam_distances) < R :
-        # projected_points_interior = sphere_centres - sphere_beam_distances * beam_series_halfspaces[:,0:3] - beam_series_halfspaces[:,0:3]*(R/1000.)
-        # if self.contains(projected_points_interior): add
-        # np.min( np.abs( beam_series_halfspaces[:,0:3].dot(projected_points.T) + planes_origin_distances ) ) == 0
 
-        return mask
+        R = sphere_radius.reshape(1, sphere_radius.shape[0])
+        not_candidates = np.zeros((len(sample_times), R.shape[1]), dtype=bool)
+        for i,time in enumerate(sample_times):
+            halfspaces = ConvexHull( inverse_rigid_body_motion( self.vertices.T, time=time ).T ).equations
+            halfspaces = np.unique(halfspaces.round(decimals=6), axis=0) 
+            normals    = halfspaces[:, 0:3]
+            distances  = halfspaces[:, 3].reshape(normals.shape[0],1)
+            sphere_beam_distances  = normals.dot(sphere_centres.T) + distances
+            not_candidates[i,:]   += np.any( sphere_beam_distances > R, axis=0)
+
+        return ~not_candidates, sample_times
 
     def _get_proximity_intervals(
             self,
             sphere_centres,
             sphere_radius,
-            rigid_body_motion,
-            collision_detection='approximate'):
-        """Compute the parametric intervals t=[[t_1,t_2],[t_3,t_4],..] in which spheres are interesecting beam.
+            rigid_body_motion):
+            """Compute the parametric intervals t=[[t_1,t_2],[t_3,t_4],..] in which spheres are interesecting beam.
 
-        This method can be used as a pre-checker before running the `intersect()` method on a polyhedral
-        set. This avoids wasting compute resources on polyhedra which clearly do not intersect the beam.
+            This method can be used as a pre-checker before running the `intersect()` method on a polyhedral
+            set. This avoids wasting compute resources on polyhedra which clearly do not intersect the beam.
 
-        Args:
-            sphere_centres (:obj:`numpy array`): Centroids of a spheres ``shape=(3,n)``.
-            sphere_radius (:obj:`numpy array`): Radius of a spheres ``shape=(n,)``.
-            rigid_body_motion (:obj:`xrd_simulator.motion.RigidBodyMotion`): Rigid body motion object describing the
-                polycrystal transformation as a function of time on the domain time=[0,1].
-            collision_detection (:obj:`string`): Optional keyword specifying the use of fast approximate collision detection. 
-                One of either exact or approximate. Defaults to approximate.
+            Args:
+                sphere_centres (:obj:`numpy array`): Centroids of a spheres ``shape=(3,n)``.
+                sphere_radius (:obj:`numpy array`): Radius of a spheres ``shape=(n,)``.
+                rigid_body_motion (:obj:`xrd_simulator.motion.RigidBodyMotion`): Rigid body motion object describing the
+                    polycrystal transformation as a function of time on the domain time=[0,1].
 
-        Returns:
-            (:obj:`list` of :obj:`list` of :obj:`list`): Parametric ranges in which the spheres
-            has an intersection with the beam. [(:obj:`None`)] if no intersection exist in ```time=[0,1]```.
-            the entry at ```[i][j]``` is a list with two floats ```[t_1,t_2]``` and gives the j:th
-            intersection interval of sphere number i with the beam.
+            Returns:
+                (:obj:`list` of :obj:`list` of :obj:`list`): Parametric ranges in which the spheres
+                has an intersection with the beam. [(:obj:`None`)] if no intersection exist in ```time=[0,1]```.
+                the entry at ```[i][j]``` is a list with two floats ```[t_1,t_2]``` and gives the j:th
+                intersection interval of sphere number i with the beam.
 
-        """
-        if collision_detection=='approximate':
-            candidate_mask = self._get_candidate_spheres(sphere_centres,
+            """
+            # TODO: better unit tests.
+            candidate_mask, sample_times  = self._get_candidate_spheres(sphere_centres,
                                                          sphere_radius,
                                                          rigid_body_motion)
-        elif collision_detection=='exact':
-            candidate_mask = np.ones(sphere_centres.shape[0], dtype=bool)
-        else:
-            raise ValueError("collision_detection must be one of -approximate- or -exact-")
 
-        beam_halfplane_normals = self.halfspaces[:, 0:3]
-        beam_halfplane_offsets = self.halfspaces[:, 3]
+            all_intersections = []
+            for j in range(candidate_mask.shape[1]):
+                if np.sum(candidate_mask[:,j])==0:
+                    merged_intersection = [None]
+                    all_intersections.append(merged_intersection)
+                else:
+                    counting = False
+                    merged_intersection = []
 
-        # Precomputable factors for root equations independent of sphere data.
-        a0 = rigid_body_motion.rotator.K.dot(beam_halfplane_normals.T)
-        a1 = rigid_body_motion.rotator.K2.dot(beam_halfplane_normals.T)
-        a2 = -beam_halfplane_normals.dot(rigid_body_motion.translation)
+                    i = 0
+                    if candidate_mask[i,j] and not counting:
+                        merged_intersection.append( [sample_times[i], sample_times[i+1]] )
+                        counting = True
 
-        # Here we will store all intersections for all spheres.
-        all_intersections = []
+                    for i in range(1, candidate_mask.shape[0]-1):
 
-        for i in range(sphere_centres.shape[0]):
+                            if candidate_mask[i,j] and not counting:
+                                merged_intersection.append( [sample_times[i-1], sample_times[i+1]] )
+                                counting = True
+                            elif not candidate_mask[i,j] and counting:
+                                merged_intersection[-1][1] = sample_times[i+1]
+                                counting = False
 
-            if not candidate_mask[i]:
-                merged_intersections = [None]
-                all_intersections.append(merged_intersections)
-                continue
+                    i = candidate_mask.shape[0] - 1
+                    if candidate_mask[i,j] and not counting:
+                        merged_intersection.append( [sample_times[i-1], sample_times[i]] )
+                    elif candidate_mask[i,j] and counting:
+                        merged_intersection[-1][1] = sample_times[i]
 
-            # Here we will store all intersections for the current sphere.
-            merged_intersections = [[0., 1.]]
+                    all_intersections.append(merged_intersection)
 
-            for p in range(self.halfspaces.shape[0]):
-
-                # Intersections for the sphere attached to halfplane number p.
-                new_intersections = []
-
-                # Define the function we seek to have intervals in s fulfilling
-                # : intersection_function(s) < 0
-                q_0 = sphere_centres[i].dot(a0[:, p])
-                q_1 = -sphere_centres[i].dot(a1[:, p])
-                q_2 = a2[p]
-                q_3 = sphere_centres[i].dot(
-                    beam_halfplane_normals[p, :]) - q_1 - sphere_radius[i] + beam_halfplane_offsets[p]
-
-                def intersection_function(time):
-                    return q_0 * np.sin(time * rigid_body_motion.rotation_angle) + q_1 * np.cos(
-                        time * rigid_body_motion.rotation_angle) + time * q_2 + q_3
-
-                # Find roots of intersection_function(s) by first finding its
-                # extremal points
-                brackets = self._find_brackets_of_roots(
-                    q_0, q_1, q_2, rigid_body_motion.rotation_angle, intersection_function)
-
-                # Find roots numerically on the intervals
-                roots = [
-                    root_scalar(
-                        intersection_function,
-                        method='bisect',
-                        bracket=bracket,
-                        maxiter=50).root for bracket in brackets]
-                roots.extend([0., 1.])
-                interval_ends = np.sort(np.array(roots))
-                fvals = intersection_function(
-                    (interval_ends[0:-1] + interval_ends[1:]) / 2.)
-
-                if np.sum(fvals > 0) == len(fvals):
-                    # Always on the exterior of the beam halfplane
-                    merged_intersections = [None]
-                    break
-
-                if np.sum(fvals < 0) == len(fvals):
-                    new_intersections.append([0., 1.])
-                    continue  # Always on the interior of the beam halfplane
-
-                # Add in all intervals where intersection_function(s) < 0
-                for k in range(len(interval_ends) - 1):
-                    if fvals[k] < 0:
-                        new_intersections.append(
-                            [interval_ends[k], interval_ends[k + 1]])
-                    else:
-                        pass
-
-                # Clean up the interval set, the sphere must intersect all
-                # halfplanes for a shared interval in s.
-                merged_intersections = self._merge_intersections(
-                    merged_intersections, new_intersections)
-
-                # The sphere will never intersect if no shared intervals are
-                # found.
-                if len(merged_intersections) == 0:
-                    break
-
-            all_intersections.append(merged_intersections)
-
-        return all_intersections
-
-    def _find_brackets_of_roots(self, q_0, q_1, q_2, rotation_angle, function):
-        """Find all sub domains on time=[0,1] which are guaranteed to hold a root of function.
-        """
-        rho_0 = np.array([rotation_angle * q_0])
-        rho_1 = np.array([-rotation_angle * q_1])
-        rho_2 = np.array([q_2])
-        time_1, time_2 = laue.find_solutions_to_tangens_half_angle_equation(
-            rho_0, rho_1, rho_2, np.array([rotation_angle]))
-        search_intervals = np.array(
-            [s for s in [0, time_1[0], time_2[0], 1] if ~np.isnan(s)])
-        f = function(search_intervals)
-        indx = 0
-        brackets = []
-        for i in range(len(search_intervals) - 1):
-            if np.sign(f[indx]) != np.sign(f[i + 1]):
-                brackets.append(
-                    [search_intervals[indx], search_intervals[i + 1]])
-                indx = i + 1
-        return brackets
-
-    def _merge_intersections(self, merged_intersections, new_intersections):
-        """Return intersection of merged_intersections and new_intersections
-
-        NOTE: Assumes no overlaps inside the brackets of merged_intersections
-        and new_intersections. I.e they are minimal descriptions of their
-        respective point sets.
-        """
-        new_intervals = []
-        for mi in merged_intersections:
-            for ni in new_intersections:
-                intersection = self._intersection(mi, ni)
-                if intersection is not None:
-                    new_intervals.append(intersection)
-        return new_intervals
-
-    def _intersection(self, bracket1, bracket2):
-        """Find the intersection between two simple bracket intervals of form [start1,end1] and [start2,end2].
-        """
-        if (bracket2[0] <= bracket1[0] <= bracket2[1]) or (
-                bracket2[0] <= bracket1[1] <= bracket2[1]):
-            points = np.sort(
-                [bracket2[0], bracket1[0], bracket2[1], bracket1[1]])
-            return [points[1], points[2]]
-        else:
-            return None
+            return all_intersections