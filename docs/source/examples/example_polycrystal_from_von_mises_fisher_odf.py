f077b198de023f01cd65fa1ef2dc928681355907
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2022-09-16 08:03:16 +0200

    doc fixes

---
+++ docs/source/examples/example_polycrystal_from_von_mises_fisher_odf.py
@@ -0,0 +1,11 @@
+from scipy.special import iv
+import numpy as np
+# Von Mises-Fisher orientation distribution function.
+# https://en.wikipedia.org/wiki/Von_Mises%E2%80%93Fisher_distribution
+def ODF(x, q):
+    kappa = 0.064 * x.dot(x) + 0.001
+    mu    = np.array([ 0.,  0.95689793,  0.28706938, -0.0440173 ])
+    p     = 4.0
+    I     = iv( p/2 - 1, kappa)
+    Cp    = kappa**(p/2 - 1) / ( (2*np.pi)**(p/2) * I )
+    return Cp * ( np.exp( kappa * np.dot( q, mu ) ) + np.exp( -(kappa * np.dot( q, mu )) ) )
