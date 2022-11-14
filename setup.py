c9063f9ea5124e7ecef4a7e38954d35c534746bb
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2022-11-14 11:46:08 +0100

    Bumped version to 0.4.1 and released to pypi.org

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xrd_simulator",
-    version="0.4.0",
+    version="0.4.1",
     author="Axel Henningsson",
     author_email="nilsaxelhenningsson@gmail.com",
     description="Tools for diffraction simulation of s3dxrd type experiments.",
