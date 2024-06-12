00aea38440a86a3f91920a765ca96f13a07af822
Marc Raventós <marcraven@gmail.com>
2024-06-12 14:48:05 +0200

    minor docstring fix

--- xrd_simulator/scattering_unit.py
+++ xrd_simulator/scattering_unit.py
@@ -4,11 +4,11 @@ For advanced users it can be usefull to interact with the scattering units, this
 from the polycrystal.
 
 """
+
 import numpy as np
 
 
 class ScatteringUnit(object):
-
     """Defines a scattering region in space as a single crystal as a convex polyhedra.
 
     The scattering unit is described in laboratory coordinates.
@@ -33,7 +33,7 @@ class ScatteringUnit(object):
         incident_polarization_vector (:obj:`numpy array`): Unit vector of linear polarization ```shape=(3,)```
         rotation_axis (:obj:`numpy array`): Sample motion rotation axis ```shape=(3,)```
         time (:obj:`numpy array`): Parametric value in range [0,1] determining at which instance during sample
-            motion the scattering ozd2,ydccured.
+            motion the scattering occured.
         phase (:obj:`xrd_simulator.phase.Phase`): The phase of the scattering unit.
         hkl_indx (:obj:`int`): Index of Miller index in the `phase.miller_indices` list.
         element_index (:obj:`int`): Index of mesh tetrahedral element refering to a `xrd_simulator.polycrystal.Polycrystal`
@@ -41,20 +41,20 @@ class ScatteringUnit(object):
     """
 
     def __init__(
-            self,
-            convex_hull,
-            scattered_wave_vector,
-            incident_wave_vector,
-            wavelength,
-            incident_polarization_vector,
-            rotation_axis,
-            time,
-            phase,
-            hkl_indx,
-            element_index,
-            zd=None,
-            yd=None
-            ):
+        self,
+        convex_hull,
+        scattered_wave_vector,
+        incident_wave_vector,
+        wavelength,
+        incident_polarization_vector,
+        rotation_axis,
+        time,
+        phase,
+        hkl_indx,
+        element_index,
+        zd=None,
+        yd=None,
+    ):
         self.convex_hull = convex_hull
         self.scattered_wave_vector = scattered_wave_vector
         self.incident_wave_vector = incident_wave_vector
@@ -91,38 +91,36 @@ class ScatteringUnit(object):
 
     @property
     def lorentz_factor(self):
-        """Compute the Lorentz intensity factor for a scattering_unit.
-        """
+        """Compute the Lorentz intensity factor for a scattering_unit."""
         k = self.incident_wave_vector
         kp = self.scattered_wave_vector
-        theta = np.arccos(k.dot(kp) / (np.linalg.norm(k)**2)) / 2.
-        korthogonal = kp - (k * kp.dot(k) / (np.linalg.norm(k)**2))
+        theta = np.arccos(k.dot(kp) / (np.linalg.norm(k) ** 2)) / 2.0
+        korthogonal = kp - (k * kp.dot(k) / (np.linalg.norm(k) ** 2))
         eta = np.arccos(
-            self.rotation_axis.dot(korthogonal) /
-            np.linalg.norm(korthogonal))
+            self.rotation_axis.dot(korthogonal) / np.linalg.norm(korthogonal)
+        )
         tol = 0.5
-        if np.abs(np.degrees(eta)) < tol or np.abs(np.degrees(eta)) > 180-tol or np.degrees(theta) < tol:
+        if (
+            np.abs(np.degrees(eta)) < tol
+            or np.abs(np.degrees(eta)) > 180 - tol
+            or np.degrees(theta) < tol
+        ):
             return np.inf
         else:
-            return 1. / (np.sin(2 * theta) * np.abs(np.sin(eta)))
+            return 1.0 / (np.sin(2 * theta) * np.abs(np.sin(eta)))
 
     @property
     def polarization_factor(self):
-        """Compute the Polarization intensity factor for a scattering_unit.
-        """
-        khatp = self.scattered_wave_vector / \
-            np.linalg.norm(self.scattered_wave_vector)
-        return 1 - np.dot(self.incident_polarization_vector, khatp)**2
+        """Compute the Polarization intensity factor for a scattering_unit."""
+        khatp = self.scattered_wave_vector / np.linalg.norm(self.scattered_wave_vector)
+        return 1 - np.dot(self.incident_polarization_vector, khatp) ** 2
 
     @property
     def centroid(self):
-        """centroid (:obj:`numpy array`): centroid of the scattering region. ``shape=(3,)``
-        """
-        return np.mean(
-            self.convex_hull.points[self.convex_hull.vertices], axis=0)
+        """centroid (:obj:`numpy array`): centroid of the scattering region. ``shape=(3,)``"""
+        return np.mean(self.convex_hull.points[self.convex_hull.vertices], axis=0)
 
     @property
     def volume(self):
-        """volume (:obj:`float`): volume of the scattering region volume
-        """
+        """volume (:obj:`float`): volume of the scattering region volume"""
         return self.convex_hull.volume
