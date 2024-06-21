6e04f3d17ce0955a7e6584a3f4b754f1ab731242
Marc Raventós <marcraven@gmail.com>
2024-06-21 14:25:04 +0200

    Pytorch partially implemented through _diffract

--- xrd_simulator/cuda.py
+++ xrd_simulator/cuda.py
@@ -1,18 +1,18 @@
 import torch
-
+import numpy as np
+import pandas as pd
 # Default to False
-use_cuda = False
+frame = np
+
 
 # ===============================================
 try:
     # Check if CUDA is available
     if torch.cuda.is_available():
-        use_cuda = True
+        frame = torch
         print("CUDA is available and GPUs are found.")
     else:
         print("CUDA is not available.")
 except Exception as e:
     print("An error occurred while checking for CUDA. Exception:", e)
 
-# Print final status
-print("use_cuda =", use_cuda)
