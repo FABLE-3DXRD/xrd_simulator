914284d9b422116554b2d6b7fec448bdbd1830bc
Marc Raventós <marcraven@gmail.com>
2024-10-18 13:30:24 +0200

    torch-cpu faster than numpy. numpy can be removed for simplicity

--- xrd_simulator/cuda.py
+++ xrd_simulator/cuda.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import pandas as pd
 # Default to False
-fw = np
+fw = torch
 
 
 # ===============================================
@@ -12,11 +12,10 @@ try:
         print("CUDA is available and GPUs are found.")
         gpu = input("Do you want to run in GPU? [y/n]").strip().lower() or 'y'
         if gpu == 'y':
-            fw = torch
             torch.set_default_device('cuda')
             print("Running in GPU...") 
         else:
-            fw = np
+            torch.set_default_device('cpu')
             print("Running in CPU...")      
     else:
         print("CUDA is not available.")
