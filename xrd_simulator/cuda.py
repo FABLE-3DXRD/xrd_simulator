eec0fe961804e6dd637b8ef67eb814f16233b2bf
Marc Raventós <marcraven@gmail.com>
2025-03-28 15:22:46 +0100

    working on memory limits for batch processing

--- xrd_simulator/cuda.py
+++ xrd_simulator/cuda.py
@@ -1,21 +1,21 @@
 import torch
+
 # Default to False
 
 # ===============================================
-torch.set_default_device('cpu')
+torch.set_default_device("cpu")
 torch.no_grad()
 try:
     # Check if CUDA is available
     if torch.cuda.is_available():
         print("CUDA is available and GPUs are found.")
-        gpu = 'n'#input("Do you want to run in GPU? [y/n]").strip().lower() or 'y'
-        if gpu == 'y':
-            torch.set_default_device('cuda')
-            print("Running in GPU...") 
+        gpu = "n"  # input("Do you want to run in GPU? [y/n]").strip().lower() or 'y'
+        if gpu == "y":
+            torch.set_default_device("cuda")
+            print("Running in GPU...")
         else:
-            print("Running in CPU...")      
+            print("Running in CPU...")
     else:
         print("CUDA is not available.")
 except Exception as e:
     print("An error occurred while checking for CUDA. Exception:", e)
-
